28d2832c1b19e6b708f76da0f529d1d1b17e1b8c
Darrshan1905 <darrshan@mobicip.com>
2024-02-12 19:15:24 +0530

    completed the websocket client and server almost

--- chat-app/ws-server.c
+++ chat-app/ws-server.c
@@ -6,31 +6,111 @@
 #define MAX_CLIENTS 100
 
 struct lws *client_list[MAX_CLIENTS];
+char *username_list[MAX_CLIENTS];
+
+void send_pvt_msg(char *sender, char *recvr, char *msg) {
+	for(int i = 0; i < MAX_CLIENTS; ++i) {
+		if(client_list[i] && strcmp(username_list[i], recvr) == 0) {
+			lws_write(client_list[i], msg, strlen(msg), LWS_WRITE_TEXT);
+		}
+	}
+}
+
+void send_active_list(struct lws *wsi) {
+	char buff[2048] = "\n---------ACTIVE USERS LIST---------\n";
+
+	for(int i = 0; i < MAX_CLIENTS; i++) {
+                if(client_list[i] && client_list[i] != wsi) {
+                        strcat(buff, username_list[i]);
+                        strcat(buff, "\n");
+                }
+        }
+
+	lws_write(wsi, buff, strlen(buff), LWS_WRITE_TEXT);
+}
+
+void send_message(char *sender, char *buff) {
+	 for (int i = 0; i < MAX_CLIENTS; ++i) {
+         	if (client_list[i] && strcmp(username_list[i], sender) != 0) {
+                	lws_write(client_list[i], buff, strlen(buff), LWS_WRITE_TEXT);
+                }
+         }
+
+}
 
 int callback_echo(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len)
 {
     	switch (reason)
     	{
         	case LWS_CALLBACK_ESTABLISHED:
-            		printf("Client connected\n");
+		{
+			char buff[2048];
+			int rv = lws_hdr_copy(wsi, buff, sizeof(buff), WSI_TOKEN_HTTP_URI_ARGS);
+			
+			char *username_start = strstr(buff, "username=");
+			username_start += strlen("username=");
 
-            		// Add client to the client list
-            		for (int i = 0; i < MAX_CLIENTS; ++i) {
-                		if (!client_list[i]) {
-                    			client_list[i] = wsi;
-                    			break;
-                		}
-            		}
+			char *username_end = strchr(username_start, '&');
+			if(!username_end)
+				username_end = strchr(username_start, '\0');
+			
+			int username_length = username_end - username_start;
+			
+			char *username = malloc(username_length);
+			strncpy(username, username_start, username_length);
+			username[username_length] = '\0';
+
+			printf("%s joined the chat\n", username);
+			
+			if(username) {				
+				// Add client to the client list
+                       		for (int i = 0; i < MAX_CLIENTS; ++i) {
+                               		if (!client_list[i]) {
+                       	        	        client_list[i] = wsi;
+						username_list[i] = username;
+               	                	        break;
+					}
+                        	}
+			}
             		break;
+		}
 
-        	case LWS_CALLBACK_RECEIVE:
+        	case LWS_CALLBACK_RECEIVE:{
+			char sender[30];
+			char *buff = (char*)in;
            		 // Forward the received message to all other clients
+			for (int i = 0; i < MAX_CLIENTS; ++i) {
+                                if (client_list[i] && client_list[i] == wsi) {
+                                        strcpy(sender, username_list[i]);
+                                }
+                        }
+                        
+			printf("%s\n", sender);
+
+			char *to;
+                        if(to = strstr(buff, "To:")) {
+                                printf("%s -> %s\n", sender, buff);
+                                char *sc = strstr(to + 4, ":");
+                                *sc = '\0';
+                                send_pvt_msg(sender, to + 4, sc + 1);
+                         }
+                         else if(strcmp(buff, "online") == 0) {
+				printf("%s\n", buff);
+                                send_active_list(wsi);
+                         }
+                         else {
+                                send_message(sender, buff);
+                                printf("%s -> %s\n", sender, buff);
+                         }
+
+
             		for (int i = 0; i < MAX_CLIENTS; ++i) {
-                		if (client_list[i] && client_list[i] != wsi) {
+                		if (client_list[i] && strcmp(username_list[i], sender) != 0) {
                     			lws_write(client_list[i], in, len, LWS_WRITE_TEXT);
                 		}
             		}
             		break;
+		}
 
         	case LWS_CALLBACK_CLOSED:
             		printf("Client disconnected\n");
@@ -55,7 +135,7 @@ static struct lws_protocols protocols[] = {
     	{
         	"echo-protocol",
         	callback_echo,
-        	0,
+        	sizeof(char[30]),
         	100, // Maximum number of bytes for the rx buffer
     	},
     	{ NULL, NULL, 0, 0 } /* terminator */
